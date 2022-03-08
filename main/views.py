835bcdaed8eead3d3444a22e5e5423bb510df29d
Vlad <isprodigy@mail.ru>
2022-03-08 18:58:05 +0300

    Student and teacher pages

--- main/views.py
+++ main/views.py
@@ -27,12 +27,18 @@ def groupInfo(request, groupName):
 
 
 def teacherInfo(request, pk):
-    context = {}
+    context = {'group': False}
     if not request.user.is_authenticated:
         return redirect('authentication')
     else:
         teacher = Teacher.objects.get(id = pk)
         context.update({'teacher': teacher})
+
+        try:
+            group = Group.objects.get(groupTeacher = teacher.id)
+            context['group'] = group
+        except:
+            pass
         return render(request, 'teacher-info.html', context)
 
 
@@ -42,6 +48,10 @@ def studentInfo(request, username):
         return redirect('authentication')
     else:
         person = CustomUser.objects.get(username = username)
-        student = Student.objects.get(id = person.id)
+        student = CustomUser.objects.get(id = person.id)
+        studentGroup = Student.objects.get(student = student).group
+        groupTeacher = Student.objects.get(student = student).group
         context.update({'student': student})
+        context.update({'studentGroup': studentGroup})
+        context.update({'groupTeacher': groupTeacher})
         return render(request, 'student-info.html', context)
